bb55a99a003a763f2a2129c8491cc9f62a9392bd
clover <clover@solidyear.com.tw>
2016-05-22 18:28:45 +0800

    add .gitignore

--- fakeDBfile.py
+++ fakeDBfile.py
@@ -17,6 +17,6 @@ for x in range(100):
     u = User.objects.create(
         first_name=fake.first_name(),
         last_name=fake.last_name(),
-        weight=random.randint(40, 150)
+        weight=random.randint(40, 150)  # weight : between 40 ~ 150
     )
     u.save()
